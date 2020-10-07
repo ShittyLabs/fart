1ad584925f839c39edc66225ba46d0674d781165
Damjan <damjan.dimitrov@actual-it.si>
2020-10-07 15:05:20 +0200

    Implement tests for Max extension method

---
+++ test/max_test.dart
@@ -0,0 +1,18 @@
+import 'package:collection/collection.dart';
+import 'package:test/test.dart';
+import 'package:fart/fart.dart';
+
+void main() {
+  group('Max', () {
+    test('should work for non-empty collection of ints', () {
+      // arrange
+      final numbersOneThroughFour = Iterable<int>.generate(4).map((n) => n + 1);
+
+      // act
+      final actual = numbersOneThroughFour.Max();
+
+      // assert
+      assert(actual == 4));
+    });
+  });
+}
