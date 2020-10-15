dcd69906fa985c59f5d4b86d51937cc2c557f76d
Patrick Tone <patrick@madronesoftware.com>
2020-10-15 12:45:02 +0200

    Adds `Max1`

--- lib/src/extension/Max.dart
+++ lib/src/extension/Max.dart
@@ -1,11 +1,31 @@
-extension MaxFart<T extends num> on Iterable<T> {
-  T Max() {
-  var max =  (this?.isEmpty ?? true) ? null : this.first;
-    for (final item in this) {
-      if (item > max) {
-          max = item;
-      }
+import 'package:fart/fart.dart';
+import 'package:fart/src/exception/ArgumentNullException.dart';
+import 'package:fart/src/exception/InvalidOperationException.dart';
+
+extension MaxFart<TSource extends num> on Iterable<TSource> {
+  TSource Max() => Max1<TSource>((number) => number);
+}
+
+extension MaxFart1<TSource> on Iterable<TSource> {
+  TResult Max1<TResult extends num>(TResult Function(TSource) selector) {
+    if (this == null) {
+      throw ArgumentNullException('`source` cannot be `null`');
     }
-    return max;
+
+    if (selector == null) {
+      throw ArgumentNullException('`selector` cannot be `null`');
+    }
+
+    if (isEmpty) {
+      throw InvalidOperationException('`souce` cannot be empty');
+    }
+
+    final max = (TResult max, TSource next) {
+      final value = selector(next);
+
+      return value > max ? value : max;
+    };
+
+    return skip(1).Aggregate(selector(first), max);
   }
 }
--- test/max_test.dart
+++ test/max_test.dart
@@ -1,18 +1,121 @@
-import 'package:collection/collection.dart';
+import 'package:fart/src/exception/ArgumentNullException.dart';
+import 'package:fart/src/exception/InvalidOperationException.dart';
 import 'package:test/test.dart';
 import 'package:fart/fart.dart';
 
 void main() {
   group('Max', () {
-    test('should work for non-empty collection of ints', () {
-      // arrange
-      final numbersOneThroughFour = Iterable<int>.generate(4).map((n) => n + 1);
+    group('Success Scenarios', () {
+      test('should work for non-empty collection of ints', () {
+        // arrange
+        final numbersOneThroughFour =
+            Iterable<int>.generate(4).map((n) => n + 1);
+        const expected = 4;
 
-      // act
-      final actual = numbersOneThroughFour.Max();
+        // act
+        final actual = numbersOneThroughFour.Max();
 
-      // assert
-      assert(actual == 4);
+        // assert
+        expect(actual, expected);
+      });
+
+      test('should return the largest value in the sequence', () {
+        // arrange
+        const doubles = [1.5E+104, 9E+103, -2E+103];
+        const expected = 1.5E+104;
+
+        // act
+        final actual = doubles.Max();
+
+        // assert
+        expect(actual, expected);
+      });
+    });
+
+    group('Error Scenarios', () {
+      test('should throw an `ArgumentNullException` if the `source` is `null`',
+          () {
+        // arrange
+        final List<int> input = null;
+
+        // act
+        // assert
+        expect(
+            () => input.Max(), throwsA(TypeMatcher<ArgumentNullException>()));
+      });
+
+      test(
+          'should throw an `InvalidOperationException` if the `source` is is empty',
+          () {
+        // arrange
+        final input = <int>[];
+
+        // act
+        // assert
+        expect(() => input.Max(),
+            throwsA(TypeMatcher<InvalidOperationException>()));
+      });
     });
   });
+
+  group('Max1', () {
+    group('Success Scenarios', () {
+      test('should return the longest pet age and name length', () {
+        // arrange
+        final pets = [Pet('Barley', 8), Pet('Boots', 4), Pet('Whiskers', 1)];
+        final agePlusNameLength = (Pet pet) => pet.age + pet.name.length;
+        const expected = 14;
+
+        // act
+        final actual = pets.Max1(agePlusNameLength);
+
+        // arrange
+        expect(actual, expected);
+      });
+    });
+
+    group('Error Scenarios', () {
+      test('should throw an `ArgumentNullException` if the `source` is `null`',
+          () {
+        // arrange
+        final List<String> input = null;
+
+        // act
+        // assert
+        expect(() => input.Max1((s) => s.length),
+            throwsA(TypeMatcher<ArgumentNullException>()));
+      });
+
+      test(
+          'should throw an `ArgumentNullException` if the `selector` is `null`',
+          () {
+        // arrange
+        final input = ['hello', 'world'];
+
+        // act
+        // assert
+        expect(() => input.Max1(null),
+            throwsA(TypeMatcher<ArgumentNullException>()));
+      });
+
+      test(
+          'should throw an `InvalidOperationException` if the `source` is is empty',
+          () {
+        // arrange
+        final input = <String>[];
+
+        // act
+        // assert
+        expect(() => input.Max1((s) => s.length),
+            throwsA(TypeMatcher<InvalidOperationException>()));
+      });
+    });
+  });
+}
+
+class Pet {
+  final String name;
+  final int age;
+
+  Pet(this.name, this.age);
 }
