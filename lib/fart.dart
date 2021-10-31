c524de98870b4443274ee9119e3ff9dd9663f875
Till Schröder <hi@till.red>
2021-10-31 15:51:20 +0100

    sort exports

--- lib/fart.dart
+++ lib/fart.dart
@@ -4,10 +4,10 @@
 library fart;
 
 export 'src/extension/Aggregate.dart';
-export 'src/extension/Reverse.dart';
-export 'src/extension/Where.dart';
 export 'src/extension/First.dart';
 export 'src/extension/Max.dart';
+export 'src/extension/Reverse.dart';
+export 'src/extension/Where.dart';
 export 'src/Fart.dart';
 
 // TODO: Export any libraries intended for clients of this package.
