0f207d46fe34d44eb9da17a9feb0b14daea525fd
Till <tillodoc@gmail.com>
2021-11-01 12:58:53 +0100

    add `all` function (#61)

--- lib/fart.dart
+++ lib/fart.dart
@@ -5,9 +5,10 @@ library fart;
 
 export 'src/extension/Any.dart';
 export 'src/extension/Aggregate.dart';
+export 'src/extension/All.dart';
+export 'src/extension/Max.dart';
 export 'src/extension/Reverse.dart';
 export 'src/extension/Where.dart';
-export 'src/extension/Max.dart';
 export 'src/Fart.dart';
 
 // TODO: Export any libraries intended for clients of this package.
